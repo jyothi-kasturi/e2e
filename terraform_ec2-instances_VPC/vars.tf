11abc7b01812d389e9d341dc8b2883f50cc187b7
jyothi-kasturi <jyothifeb1@gmail.com>
2020-01-06 22:42:10 +0000

    added aws full path

--- terraform_ec2-instances_VPC/vars.tf
+++ terraform_ec2-instances_VPC/vars.tf
@@ -2,11 +2,11 @@ variable "AWS_REGION" {
   default = "eu-west-1"
 }
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "~/.ssh/devopsmonks.pub"
+  default = "/home/vagrant/.ssh/devopsmonks.pub"
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "~/.ssh/devopsmonks.pem"
+  default = "/home/vagrant/.ssh/devopsmonks.pem"
 }
 
 variable "AMIS" {
